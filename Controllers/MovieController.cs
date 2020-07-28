b4db40c4f6ed3478c5aaf2f7f29efdcd95c405ac
Regan Meloche <reganmeloche@gmail.com>
2020-07-27 21:06:41 -0600

    jul 27

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoviesMVC.Models;
+
+namespace MoviesMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        List<Movie> _movieList;
+
+        public MovieController(List<Movie> movieList)
+        {
+            _movieList = movieList;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MoviesMVC.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public int Year { get; set; }
+    }
+}
